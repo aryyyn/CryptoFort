52eb7e37adb2c3ae4f2df3fe7fda68fc4902982b
aryyyn <aryan.211618@ncit.edu.np>
2023-12-30 13:30:48 +0545

    made some final changes to register UI

--- main.py
+++ main.py
@@ -8,56 +8,48 @@ from handler.showPassword import togglePassword
 app = QApplication(sys.argv)
 window = QMainWindow() 
 
-window.setMinimumSize(750, 550)
+window.setFixedSize(750, 550)
 window.setWindowTitle("CryptoFort | Register")
 icon = QIcon("logo/logo.png")
 window.setWindowIcon(icon)
 window.setStyleSheet("""
-                     
-    * {
-    color: white; 
-    background: black; 
+* {
+    color: #00FF00; 
+    background: #000000; 
 }
-                     
 
 QLineEdit {
-    border: 2px solid #C0C0C0;
+    border: 2px solid #00FF00; 
     border-radius: 8px;
     padding: 25px;
-    selection-background-color: #A9A9A9;
-    background-color: black;
-    color: white;
+    selection-background-color: #00FF00; 
+    background-color: #111111; 
+    color: #00FF00; 
     font-size: 14px;
 }
 
 QPushButton {
-    border: 2px solid #8f8f91;
+    border: 2px solid #00FF00; /* Neon green border */
     border-radius: 8px;
     background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-                                stop: 0 #EEEEEE, stop: 0.5 #DDDDDD, stop: 1 #EEEEEE);
+                                stop: 0 #111111, stop: 0.5 #222222, stop: 1 #111111);
     min-width: 100px;
     font-size: 12px;
-    color:black;
-                     
-
-   
+    color: #00FF00; /* Neon green text color */
 }
 
 QPushButton:hover {
     background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
-                                stop: 0 green, stop: 0.5 white, stop: 1 green);
+                                stop: 0 #222222, stop: 0.5 #111111, stop: 1 #222222);
 }
-                     
-                     
 
 QLabel {
-    color: white;
+    color: #00FF00; 
     font-size: 16px;
     font-weight: bold;
 }
-
-                     
 """)
+
 
 
 
@@ -71,7 +63,7 @@ layout.setSpacing(5)
 
 RegisterTitle = QLabel("CryptoFort || Register")
 RegisterTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
-RegisterTitle.setFont(QFont("Arial", 20)) #changing the font for the title
+
 
 
 email_layout = QHBoxLayout()
@@ -116,9 +108,23 @@ repassword_layout.addWidget(repassword)
 repassword_layout.addWidget(showconfirmregisterpassword)
 
 
+promptlogin_layout = QHBoxLayout()
+
+promptlogin = QLabel("Already have an account? ")
+promptlogin.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+
+promptloginbtn = QPushButton("Login")
+promptloginbtn.setFixedSize(50, 25)
+
+promptlogin_layout.addWidget(promptlogin)
+promptlogin_layout.addSpacing(2)  # Adjust the horizontal spacing here
+promptlogin_layout.addWidget(promptloginbtn, alignment=Qt.AlignmentFlag.AlignLeft)
+
+
 
 rrsubmit = QPushButton("Register")
-rrsubmit.setFixedSize(100, 50)
+rrsubmit.setFixedSize(70, 50)
 
 
 layout.addWidget(RegisterTitle)
@@ -126,6 +132,7 @@ layout.addLayout(email_layout)
 layout.addLayout(password_layout)
 layout.addLayout(repassword_layout)
 layout.addWidget(rrsubmit, alignment=Qt.AlignmentFlag.AlignCenter)
+layout.addLayout(promptlogin_layout)
 
 rrsubmit.clicked.connect(joke)
 showregisterpassword.clicked.connect(lambda: togglePassword(registerpassword))
